fc5f4e6ce6461184cb963b1e365ed9ecaf26d265
edv-Shin <edvedv613@gmail.com>
2025-05-06 16:25:48 +0900

    feat: 업데이트 요청 다이얼로그 구현
    Related to: #47

---
+++ presentation/src/main/java/com/project200/presentation/update/UpdateDialogFragment.kt
@@ -0,0 +1,56 @@
+package com.project200.presentation.update
+
+
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import com.project200.common.base.BaseDialogFragment
+import com.project200.undabang.presentation.R
+import com.project200.undabang.presentation.databinding.DialogUpdateBinding
+import androidx.core.net.toUri
+import com.project200.common.constants.AppConstants.MARKET_URL
+import androidx.core.graphics.drawable.toDrawable
+
+class UpdateDialogFragment(private val isForceUpdate: Boolean): BaseDialogFragment<DialogUpdateBinding>(R.layout.dialog_update) {
+
+    override fun getViewBinding(view: View): DialogUpdateBinding {
+        return DialogUpdateBinding.bind(view)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        isCancelable = !isForceUpdate
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        dialog?.window?.let { window ->
+            val screenWidth = resources.displayMetrics.widthPixels
+            val desiredWidth = (screenWidth * 0.85).toInt()
+
+            window.setLayout(desiredWidth, WindowManager.LayoutParams.WRAP_CONTENT)
+
+            window.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+
+        }
+    }
+
+    override fun setupViews() {
+        super.setupViews()
+
+        binding.updateBtn.setOnClickListener { navigateToMarket() }
+        binding.laterBtn.setOnClickListener { dismiss() }
+
+        binding.laterBtn.visibility = if (isForceUpdate) View.GONE else View.VISIBLE
+        binding.btnBlank.visibility = if (isForceUpdate) View.GONE else View.VISIBLE
+    }
+
+    private fun navigateToMarket() {
+        startActivity(Intent(Intent.ACTION_VIEW, MARKET_URL.toUri()))
+    }
+}
