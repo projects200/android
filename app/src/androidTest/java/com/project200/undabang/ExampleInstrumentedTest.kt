892c840da581e415f23f49ab9dddf3c7932f9550
edv-Shin <edvedv613@gmail.com>
2025-05-06 17:13:53 +0900

    fix: 테스트코드 프로젝트명 변경

--- app/src/androidTest/java/com/project200/undabang/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/project200/undabang/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.project200.myapp", appContext.packageName)
+        assertEquals("com.project200.undabang", appContext.packageName)
     }
 }
