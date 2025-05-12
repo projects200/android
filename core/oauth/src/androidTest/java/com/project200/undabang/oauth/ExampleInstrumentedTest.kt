502c2e3b1e5b4f630bdc267209c99ede2d31f513
edv-Shin <edvedv613@gmail.com>
2025-05-12 19:40:43 +0900

    fix: 테스트코드 패키지명 수정 #2

--- core/oauth/src/androidTest/java/com/project200/undabang/oauth/ExampleInstrumentedTest.kt
+++ core/oauth/src/androidTest/java/com/project200/undabang/oauth/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.project200.undabang.oauth.test", appContext.packageName)
+        assertEquals("com.project200.undabang.core.oauth.test", appContext.packageName)
     }
 }
