4a3d27dd6de46890a8652eee5930f88b26e02ecc
edv-Shin <edvedv613@gmail.com>
2025-05-14 02:57:37 +0900

    refactor: Locale 지역 명시적으로 선언 #2

--- presentation/src/main/java/com/project200/presentation/utils/DatePickerDialogFragment.kt
+++ presentation/src/main/java/com/project200/presentation/utils/DatePickerDialogFragment.kt
@@ -1,19 +1,15 @@
 package com.project200.presentation.utils
 
-import android.annotation.SuppressLint
 import android.view.View
 import com.project200.presentation.base.BaseDialogFragment
-// DatePickerDialogFragment가 사용하는 레이아웃(R.layout.dialog_date_picker)이 있는 모듈의 R 클래스를 임포트해야 합니다.
-// 만약 presentation 모듈에 있다면 아래와 같이 사용합니다.
-import com.project200.undabang.presentation.R // 또는 com.project200.presentation.R 등 실제 경로로 수정
-import com.project200.undabang.presentation.databinding.DialogDatePickerBinding // 바인딩 클래스 경로 확인
-import timber.log.Timber
-import java.time.LocalDate // LocalDate 임포트
-import java.time.format.DateTimeParseException // DateTimeParseException 임포트
+import com.project200.undabang.presentation.R
+import com.project200.undabang.presentation.databinding.DialogDatePickerBinding
+import java.time.LocalDate
 import java.util.Calendar
+import java.util.Locale
 
 class DatePickerDialogFragment(
-    private val initialDateString: String? = null, // 초기 날짜 문자열을 받을 파라미터 추가
+    private val initialDateString: String? = null,
     private val onDateSelected: (String) -> Unit
 ) : BaseDialogFragment<DialogDatePickerBinding>(R.layout.dialog_date_picker) {
 
@@ -21,7 +17,6 @@ class DatePickerDialogFragment(
         return DialogDatePickerBinding.bind(view)
     }
 
-    @SuppressLint("DefaultLocale")
     override fun setupViews() = with(binding) {
         val today = Calendar.getInstance()
         datePicker.maxDate = today.timeInMillis // 오늘 이후 날짜 선택 불가
@@ -39,7 +34,7 @@ class DatePickerDialogFragment(
             val year = datePicker.year
             val month = datePicker.month
             val day = datePicker.dayOfMonth
-            onDateSelected(String.format("%04d-%02d-%02d", year, month + 1, day))
+            onDateSelected(String.format(Locale.KOREA,"%04d-%02d-%02d", year, month + 1, day))
             dismiss()
         }
     }
