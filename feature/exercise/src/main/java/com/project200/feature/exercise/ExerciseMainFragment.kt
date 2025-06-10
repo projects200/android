57e9cb86b52bc561481ebec1c71509a429c13807
edv-Shin <edvedv613@gmail.com>
2025-06-11 03:14:29 +0900

    feat: 운동 메인화면 네비게이션 연결 #36

---
+++ feature/exercise/src/main/java/com/project200/feature/exercise/ExerciseMainFragment.kt
@@ -0,0 +1,25 @@
+package com.project200.feature.exercise
+
+import android.view.View
+import com.project200.presentation.base.BindingFragment
+import com.project200.presentation.navigator.FragmentNavigator
+import com.project200.undabang.feature.exercise.R
+import com.project200.undabang.feature.exercise.databinding.FragmentExerciseMainBinding
+
+class ExerciseMainFragment: BindingFragment<FragmentExerciseMainBinding>(R.layout.fragment_exercise_main) {
+    private var fragmentNavigator: FragmentNavigator? = null
+    override fun getViewBinding(view: View): FragmentExerciseMainBinding {
+        return FragmentExerciseMainBinding.bind(view)
+    }
+
+    override fun setupViews() {
+        binding.root.setOnClickListener {
+            fragmentNavigator?.navigateFromExerciseMainToExerciseList()
+        }
+    }
+
+    override fun onDetach() {
+        fragmentNavigator = null
+        super.onDetach()
+    }
+}
--- presentation/src/main/java/com/project200/presentation/navigator/FragmentNavigator.kt
+++ presentation/src/main/java/com/project200/presentation/navigator/FragmentNavigator.kt
@@ -1,9 +1,14 @@
 package com.project200.presentation.navigator
 
 interface FragmentNavigator {
+    /** 운동 리스트 */
     fun navigateFromExerciseListToExerciseDetail(recordId: Long)
     fun navigateFromExerciseListToSetting()
     fun navigateFromExerciseListToExerciseForm()
+    /** 운동 상세 */
     fun navigateFromExerciseDetailToExerciseForm(recordId: Long)
+    /** 운동 생성/수정 */
     fun navigateFromExerciseFormToExerciseDetail(recordId: Long)
+    /** 운동 메인 */
+    fun navigateFromExerciseMainToExerciseList()
 }
