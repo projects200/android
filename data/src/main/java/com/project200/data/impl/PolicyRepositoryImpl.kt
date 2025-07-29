bfc655d5ef04713cb4fd0cf351b0452ef0ac8fde
edv-Shin <edvedv613@gmail.com>
2025-07-29 14:30:04 +0900

    fix: PolicyDTO에 있던 예상 획득 점수 api 관련 코드 삭제 #173

--- data/src/main/java/com/project200/data/impl/PolicyRepositoryImpl.kt
+++ data/src/main/java/com/project200/data/impl/PolicyRepositoryImpl.kt
@@ -26,30 +26,4 @@ class PolicyRepositoryImpl @Inject constructor(
             mapper = { it?.toDomain() ?: throw NoSuchElementException("정책 데이터가 없습니다.") }
         )
     }
-
-    override suspend fun getExpectedScoreInfo(): BaseResult<ExpectedScoreInfo> {
-        // TODO 실제 예상 점수 획득 정보 조회 api
-
-        // 더미 데이터 생성
-        val now = LocalDateTime.now()
-        val today = LocalDate.now()
-
-        val dummyExpectedScoreInfo = ExpectedScoreInfo(
-            pointsPerExercise = 3,
-            currentUserScore = 99,
-            maxScore = 100,
-            validWindow = ValidWindow(
-                startDateTime = now.minusDays(1).withHour(0).withMinute(0).withSecond(0)
-                    .withNano(0), // 2일 전 00:00:00
-                endDateTime = now
-            ),
-            earnableScoreDays = listOf(
-                today.minusDays(1) // 어제 날짜 (이미 점수를 획득했다고 가정)
-            )
-        )
-
-        return BaseResult.Success(dummyExpectedScoreInfo)
-    }
-
-
 }
--- domain/src/main/java/com/project200/domain/repository/PolicyRepository.kt
+++ domain/src/main/java/com/project200/domain/repository/PolicyRepository.kt
@@ -1,10 +1,8 @@
 package com.project200.domain.repository
 
 import com.project200.domain.model.BaseResult
-import com.project200.domain.model.ExpectedScoreInfo
 import com.project200.domain.model.PolicyGroup
 
 interface PolicyRepository {
     suspend fun getPolicyGroup(groupName: String): BaseResult<PolicyGroup>
-    suspend fun getExpectedScoreInfo(): BaseResult<ExpectedScoreInfo>
 }
