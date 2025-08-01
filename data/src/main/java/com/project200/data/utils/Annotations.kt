a5e8e31cd8f4f81dd41777d111229257e2bd0fe5
edv-Shin <edvedv613@gmail.com>
2025-08-01 23:03:05 +0900

    refactor: api path에 따른 토큰 헤더 결정 로직 어노테이션으로 변경 #173

---
+++ data/src/main/java/com/project200/data/utils/Annotations.kt
@@ -0,0 +1,25 @@
+package com.project200.data.utils
+
+/**
+ * 이 어노테이션이 붙은 Retrofit API 호출에는
+ * Authorization 헤더(토큰)를 추가하지 않습니다.
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class OpenApi
+
+/**
+ * 이 어노테이션이 붙은 Retrofit API 호출에는
+ * IdToken에 대한 Authorization 헤더(토큰)를 추가합니다.
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class IdTokenApi
+
+/**
+ * 이 어노테이션이 붙은 Retrofit API 호출에는
+ * AccessToken에 대한 Authorization 헤더(토큰)를 추가합니다.
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AccessTokenApi
