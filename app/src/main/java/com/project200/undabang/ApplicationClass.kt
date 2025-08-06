80c16571cc944b91406673ae5314b2ccad26f323
edv-Shin <edvedv613@gmail.com>
2025-08-07 03:56:58 +0900

    feat: fcm 채널 생성 #193

--- app/src/main/java/com/project200/undabang/ApplicationClass.kt
+++ app/src/main/java/com/project200/undabang/ApplicationClass.kt
@@ -1,12 +1,15 @@
 package com.project200.undabang
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class ApplicationClass: Application() {
+class ApplicationClass : Application() {
     override fun onCreate() {
         super.onCreate()
 
@@ -14,5 +17,22 @@ class ApplicationClass: Application() {
 
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
         Timber.d("Timber")
+
+        createNotificationChannel()
+    }
+
+    /**
+     * 기본 알림 채널을 생성합니다.
+     */
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            getString(R.string.fcm_channel_id),
+            getString(R.string.fcm_channel_name),
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.createNotificationChannel(channel)
     }
 }
