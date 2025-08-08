467f84c288ccccaa6bea53f58de9a538dfadf6ce
edv-Shin <edvedv613@gmail.com>
2025-08-09 00:15:06 +0900

    feat: build.gradle convention 라이브러리로 수정 #205

--- feature/timer/build.gradle.kts
+++ feature/timer/build.gradle.kts
@@ -1,43 +1,35 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    id("convention.android.library")
+    id("convention.android.hilt")
+    alias(libs.plugins.navigation.safeargs)
 }
 
 android {
-    namespace = "com.project200.undabnag.timer"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+    namespace = "com.project200.undabang.feature.timer"
 }
 
 dependencies {
+    implementation(projects.domain)
+    implementation(projects.common)
+    implementation(projects.presentation)
 
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.google.android.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.androidx.test.espresso.core)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.activity)
+
+    // Lifecycle
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    testImplementation(libs.androidx.arch.core.testing)
+    testImplementation(libs.turbine)
+    androidTestImplementation(libs.androidx.navigation.testing)
 }
--- feature/timer/src/androidTest/java/com/project200/undabnag/timer/ExampleInstrumentedTest.kt
+++ feature/timer/src/androidTest/java/com/project200/undabnag/timer/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.project200.undabnag.timer.test", appContext.packageName)
+        assertEquals("com.project200.undabnag.feature.timer.test", appContext.packageName)
     }
 }
