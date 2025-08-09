f876d49011fab89f783ab952fc8c4d199c58c969
edv-Shin <edvedv613@gmail.com>
2025-08-10 03:57:45 +0900

    feat: 타이머 리스트 ui 구현 #211

--- feature/timer/src/main/java/com/project200/feature/timer/TimerListFragment.kt
+++ feature/timer/src/main/java/com/project200/feature/timer/TimerListFragment.kt
@@ -1,11 +1,14 @@
 package com.project200.feature.timer
 
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.project200.domain.model.CustomTimer
 import com.project200.presentation.base.BindingFragment
 import com.project200.undabang.feature.timer.R
 import com.project200.undabang.feature.timer.databinding.FragmentTimerListBinding
 
 class TimerListFragment: BindingFragment<FragmentTimerListBinding>(R.layout.fragment_timer_list) {
+    private lateinit var customTimerRVAdapter: CustomTimerRVAdapter
 
     override fun getViewBinding(view: View): FragmentTimerListBinding {
         return FragmentTimerListBinding.bind(view)
@@ -13,5 +16,16 @@ class TimerListFragment: BindingFragment<FragmentTimerListBinding>(R.layout.frag
 
     override fun setupViews() {
         super.setupViews()
+
+        initClickListeners()
+    }
+
+    private fun initClickListeners() {
+        binding.simpleTimerBtn.setOnClickListener {
+
+        }
+        binding.addCustomTimerBtn.setOnClickListener {
+
+        }
     }
 }
