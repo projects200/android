2a232e9010aedd634ae23fdd6ca1b55f68efeaaf
edv-Shin <edvedv613@gmail.com>
2025-08-16 04:22:06 +0900

    feat: fcm 토큰 저장, 헤더로 전송 #220

---
+++ common/src/main/java/com/project200/common/di/StorageModule.kt
@@ -0,0 +1,42 @@
+package com.project200.common.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object StorageModule {
+
+    private const val APP_PREFS_NAME = "undabang_secure_prefs"
+
+    @Provides
+    @Singleton
+    fun provideMasterKey(@ApplicationContext context: Context): MasterKey {
+        return MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideEncryptedSharedPreferences(
+        @ApplicationContext context: Context,
+        masterKey: MasterKey
+    ): SharedPreferences {
+        return EncryptedSharedPreferences.create(
+            context,
+            APP_PREFS_NAME,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+}
