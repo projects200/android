3a17bc8ecf7a9a9fbc2b68394e40172add9e08a5
edv-Shin <edvedv613@gmail.com>
2025-08-27 00:08:58 +0900

    feat: 수정 화면으로 이동 구현 #259

--- feature/timer/src/main/java/com/project200/feature/timer/custom/CustomTimerFragment.kt
+++ feature/timer/src/main/java/com/project200/feature/timer/custom/CustomTimerFragment.kt
@@ -241,10 +241,13 @@ class CustomTimerFragment: BindingFragment<FragmentCustomTimerBinding>(R.layout.
     private fun showMenu() {
         MenuBottomSheetDialog(
             onEditClicked = {
-                // TODO: 타이머 수정 기능이 추가되면 구현 예정
+                findNavController().navigate(
+                    CustomTimerFragmentDirections.actionCustomTimerToCustomTimerFormFragment(
+                        viewModel.customTimerId
+                    )
+                )
             },
             onDeleteClicked = { showDeleteConfirmationDialog() },
-            isEditVisible = false // TODO: 타이머 수정 기능이 추가되면 제거 예정
         ).show(parentFragmentManager, MenuBottomSheetDialog::class.java.simpleName)
     }
 
