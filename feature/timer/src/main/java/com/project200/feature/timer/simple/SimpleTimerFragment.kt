c3384e2dbed6cb0fdd24eafa865066b2e52b6827
edv-Shin <edvedv613@gmail.com>
2025-08-28 11:02:51 +0900

    feat: 심플 타이머 추가/삭제 ui 구현 #261

--- feature/timer/src/main/java/com/project200/feature/timer/simple/SimpleTimerFragment.kt
+++ feature/timer/src/main/java/com/project200/feature/timer/simple/SimpleTimerFragment.kt
@@ -6,11 +6,18 @@ import android.animation.ValueAnimator
 import com.project200.feature.timer.TimePickerDialog
 import android.media.MediaPlayer
 import android.media.RingtoneManager
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.view.ContextThemeWrapper
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewTreeObserver
 import android.view.animation.LinearInterpolator
 import android.widget.PopupMenu
 import android.widget.Toast
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat.getColor
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -18,6 +25,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.project200.domain.model.SimpleTimer
+import com.project200.feature.timer.utils.MenuStyler
 import com.project200.feature.timer.utils.TimerFormatter.toFormattedTime
 import com.project200.feature.timer.utils.TimerFormatter.toFormattedTimeAsLong
 import com.project200.presentation.base.BindingFragment
@@ -25,6 +33,7 @@ import com.project200.undabang.feature.timer.R
 import com.project200.undabang.feature.timer.databinding.FragmentSimpleTimerBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class SimpleTimerFragment : BindingFragment<FragmentSimpleTimerBinding>(R.layout.fragment_simple_timer) {
@@ -57,36 +66,36 @@ class SimpleTimerFragment : BindingFragment<FragmentSimpleTimerBinding>(R.layout
     }
 
     private fun setupRecyclerView() {
+        timerAdapter = SimpleTimerRVAdapter(
+            onItemClick = { simpleTimer ->
+                stopAndResetTimer()
+                viewModel.setTimer(simpleTimer.time)
+                viewModel.startTimer()
+            },
+            onMenuClick = { simpleTimer, view -> showPopupMenu(view, simpleTimer) },
+            onAddClick = { viewModel.addTimerItem() }
+        )
         binding.simpleTimerRv.apply {
             layoutManager = GridLayoutManager(requireContext(), RV_ITEM_COL_COUNT)
-
-            // 어댑터만 미리 생성, 데이터는 옵저버에서 설정
-            timerAdapter = SimpleTimerRVAdapter(
-                onItemClick = { simpleTimer ->
-                    stopAndResetTimer()
-                    viewModel.setTimer(simpleTimer.time)
-                    viewModel.startTimer()
-                },
-                onMenuClick = { simpleTimer, view ->
-                    showPopupMenu(view, simpleTimer)
-                }
-            )
             adapter = timerAdapter
+            postponeEnterTransition() // Transition 사용 시 화면 깜빡임 방지
 
-            // onGlobalLayout 리스너를 사용해 높이를 한 번만 계산
-            viewTreeObserver.addOnGlobalLayoutListener(object :
-                ViewTreeObserver.OnGlobalLayoutListener {
+            // RecyclerView의 레이아웃이 완료된 후 높이를 계산
+            viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
                 override fun onGlobalLayout() {
-                    if (isHeightCalculated) return
+                    if (isHeightCalculated || height == 0) return
 
-                    binding.simpleTimerRv.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                    // 리스너를 즉시 제거하여 중복 호출 방지
+                    viewTreeObserver.removeOnGlobalLayoutListener(this)
+
                     val paddingInPixels = resources.getDimensionPixelSize(com.project200.undabang.presentation.R.dimen.base_horizontal_margin)
-                    val recyclerViewHeight = binding.simpleTimerRv.height - paddingInPixels
+                    val recyclerViewHeight = height - paddingInPixels
                     timerAdapter.itemHeight = recyclerViewHeight / RV_ITEM_ROW_COUNT
 
-                    // 높이 계산 후 notifyDataSetChanged() 호출
+                    // 높이 계산 후 어댑터 갱신
                     timerAdapter.notifyDataSetChanged()
                     isHeightCalculated = true
+                    startPostponedEnterTransition() // Transition 다시 시작
                 }
             })
         }
@@ -113,25 +122,22 @@ class SimpleTimerFragment : BindingFragment<FragmentSimpleTimerBinding>(R.layout
         }
 
         // ViewModel의 타이머 아이템 목록을 관찰
-        viewModel.timerItems.observe(viewLifecycleOwner) { newItems ->
-            timerAdapter.items = newItems
-            if (isHeightCalculated) {
-                timerAdapter.notifyDataSetChanged()
-            }
+        viewModel.timerItems.observe(viewLifecycleOwner) { timers ->
+            timerAdapter.submitList(timers)
         }
 
         viewLifecycleOwner.lifecycleScope.launch {
             // UI가 STARTED 상태일 때만 수집하고, STOPPED 상태가 되면 중단
             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.eventFlow.collect { event ->
-                    when (event) {
-                        is TimerEvent.NavigateToErrorScreen -> {
-                            findNavController().navigateUp()
-                        }
-                        is TimerEvent.ShowToast -> {
-                            Toast.makeText(requireContext(), getString(R.string.edit_simple_timer_error), Toast.LENGTH_SHORT).show()
-                        }
+                viewModel.toastMessage.collect { type ->
+                    if(type == SimpleTimerToastMessage.GET_ERROR) findNavController()
+                    val messageResId = when (type) {
+                        SimpleTimerToastMessage.GET_ERROR -> R.string.load_simple_timer_error
+                        SimpleTimerToastMessage.EDIT_ERROR -> R.string.edit_simple_timer_error
+                        SimpleTimerToastMessage.ADD_ERROR -> R.string.add_simple_timer_error
+                        SimpleTimerToastMessage.DELETE_ERROR -> R.string.delete_simple_timer_error
                     }
+                    Toast.makeText(requireContext(), getString(messageResId), Toast.LENGTH_SHORT).show()
                 }
             }
         }
@@ -192,24 +198,6 @@ class SimpleTimerFragment : BindingFragment<FragmentSimpleTimerBinding>(R.layout
         }
     }
 
-    private fun showPopupMenu(view: View, simpleTimer: SimpleTimer) {
-        val popupMenu = PopupMenu(requireContext(), view)
-        popupMenu.menuInflater.inflate(R.menu.timer_item_menu, popupMenu.menu) // menu_timer_item.xml을 사용
-
-        popupMenu.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.action_edit -> { // 수정 메뉴
-                    showTimePickerDialog(simpleTimer)
-                    true
-                }
-                // TODO: 삭제 기능 추가 (추가/삭제 함께 구현 예정)
-                else -> false
-            }
-        }
-        popupMenu.show()
-    }
-
-
     private fun showTimePickerDialog(simpleTimer: SimpleTimer) {
         TimePickerDialog(
             simpleTimer.time,
@@ -217,6 +205,31 @@ class SimpleTimerFragment : BindingFragment<FragmentSimpleTimerBinding>(R.layout
                 viewModel.updateTimerItem(simpleTimer.copy(time = newTime))
             }
         ).show(parentFragmentManager, TAG)
+    }
+
+    private fun showPopupMenu(view: View, simpleTimer: SimpleTimer) {
+        val contextWrapper = ContextThemeWrapper(requireContext(), R.style.PopupItemStyle)
+
+        PopupMenu(contextWrapper, view).apply {
+            menuInflater.inflate(R.menu.timer_item_menu, this.menu)
+
+            menu.findItem(R.id.action_edit)?.let {
+                MenuStyler.applyTextColor(requireContext(), it, android.R.color.black)
+            }
+            menu.findItem(R.id.action_delete)?.let {
+                MenuStyler.applyTextColor(requireContext(), it, com.project200.undabang.presentation.R.color.error_led)
+            }
+
+            setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.action_edit -> showTimePickerDialog(simpleTimer)
+                    R.id.action_delete -> viewModel.deleteTimerItem(simpleTimer.id)
+                }
+                true
+            }
+
+            MenuStyler.showIcons(this)
+        }.show()
     }
 
     override fun onDestroyView() {
--- feature/timer/src/main/java/com/project200/feature/timer/simple/SimpleTimerViewModel.kt
+++ feature/timer/src/main/java/com/project200/feature/timer/simple/SimpleTimerViewModel.kt
@@ -17,21 +17,18 @@ import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
-sealed class TimerEvent {
-    object NavigateToErrorScreen : TimerEvent()
-    data class ShowToast(val code: String) : TimerEvent()
-}
-
 @HiltViewModel
 class SimpleTimerViewModel @Inject constructor(
     private val getSimpleTimersUseCase: GetSimpleTimersUseCase,
-    private val editSimpleTimerUseCase: EditSimpleTimerUseCase
+    //private val addSimpleTimerUseCase: AddSimpleTimerUseCase,
+    private val editSimpleTimerUseCase: EditSimpleTimerUseCase,
+    //private val deleteSimpleTimerUseCase: DeleteSimpleTimerUseCase
 ): ViewModel() {
     var totalTime: Long = 0
 
     // 타이머 아이템 리스트
-    private val _timerItems = MutableLiveData<MutableList<SimpleTimer>>()
-    val timerItems: LiveData<MutableList<SimpleTimer>> = _timerItems
+    private val _timerItems = MutableLiveData<List<SimpleTimer>>()
+    val timerItems: LiveData<List<SimpleTimer>> = _timerItems
 
     private val _remainingTime = MutableLiveData<Long>()
     val remainingTime: LiveData<Long> = _remainingTime
@@ -40,8 +37,8 @@ class SimpleTimerViewModel @Inject constructor(
     val isTimerRunning: LiveData<Boolean>  = _isTimerRunning
 
     // 이벤트를 전달할 SharedFlow 생성
-    private val _eventFlow = MutableSharedFlow<TimerEvent>()
-    val eventFlow: SharedFlow<TimerEvent> = _eventFlow.asSharedFlow()
+    private val _toastMessage = MutableSharedFlow<SimpleTimerToastMessage>()
+    val toastMessage: SharedFlow<SimpleTimerToastMessage> = _toastMessage
 
     private var countDownTimer: CountDownTimer? = null
 
@@ -58,10 +55,41 @@ class SimpleTimerViewModel @Inject constructor(
                     _timerItems.value = result.data.toMutableList()
                 }
                 is BaseResult.Error -> {
-                    _eventFlow.emit(TimerEvent.NavigateToErrorScreen)
+                    _toastMessage.emit(SimpleTimerToastMessage.GET_ERROR)
                 }
             }
         }
+    }
+
+    fun addTimerItem() {
+        val currentItems = _timerItems.value ?: emptyList()
+        if (currentItems.size >= MAX_TIMER_COUNT) return
+
+        // TODO: UseCase를 통한 비동기 추가 로직 구현
+        // viewModelScope.launch {
+        //     when(val result = addSimpleTimerUseCase(DEFAULT_ADD_TIME_SEC)) {
+        //         is BaseResult.Success -> loadTimerItems() // 성공 시 리스트 새로고침
+        //         is BaseResult.Error -> _toastMessage.value = SimpleTimerToastMessage.ADD_ERROR
+        //     }
+        // }
+
+        // 임시 로직
+        val newTimer = SimpleTimer(id = System.currentTimeMillis(), time = DEFAULT_ADD_TIME_SEC)
+        _timerItems.value = currentItems + newTimer
+    }
+
+    fun deleteTimerItem(timerId: Long) {
+        // TODO: UseCase를 통한 비동기 삭제 로직 구현
+        // viewModelScope.launch {
+        //     when(val result = deleteSimpleTimerUseCase(timerId)) {
+        //         is BaseResult.Success -> loadTimerItems() // 성공 시 리스트 새로고침
+        //         is BaseResult.Error -> _toastMessage.value = SimpleTimerToastMessage.DELETE_ERROR
+        //     }
+        // }
+
+        // 임시 로직
+        val currentItems = _timerItems.value ?: return
+        _timerItems.value = currentItems.filterNot { it.id == timerId }
     }
 
 
@@ -105,17 +133,18 @@ class SimpleTimerViewModel @Inject constructor(
 
     // 타이머 아이템을 수정하는 함수
     fun updateTimerItem(updatedTimer: SimpleTimer) {
-        val currentItems = _timerItems.value ?: return
+        val currentItems = _timerItems.value?.toMutableList() ?: return
         val index = currentItems.indexOfFirst { it.id == updatedTimer.id }
+
         if (index != -1) {
             currentItems[index] = updatedTimer
-            _timerItems.value = currentItems
+            _timerItems.value = currentItems // 새 리스트 할당
 
             viewModelScope.launch {
                 val result = editSimpleTimerUseCase(updatedTimer)
                 if (result is BaseResult.Error) {
                     Timber.e("타이머 수정 실패: ${result.message}")
-                    _eventFlow.emit(TimerEvent.ShowToast(result.errorCode.toString()))
+                    _toastMessage.emit(SimpleTimerToastMessage.EDIT_ERROR)
                 }
             }
         }
@@ -128,5 +157,7 @@ class SimpleTimerViewModel @Inject constructor(
 
     companion object {
         const val COUNTDOWN_INTERVAL = 50L // 50ms
+        const val MAX_TIMER_COUNT = 6
+        const val DEFAULT_ADD_TIME_SEC = 60
     }
 }
