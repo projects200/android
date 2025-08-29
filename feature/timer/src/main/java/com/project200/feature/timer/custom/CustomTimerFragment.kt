62b24fd97bed6c8e1690c6d926cf926fe62b3320
edv-Shin <edvedv613@gmail.com>
2025-08-29 23:51:00 +0900

    feat: 안드로이드 백버튼에도 갱신 플래그 추가 #264

--- feature/timer/src/main/java/com/project200/feature/timer/custom/CustomTimerFragment.kt
+++ feature/timer/src/main/java/com/project200/feature/timer/custom/CustomTimerFragment.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.animation.LinearInterpolator
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -57,6 +58,13 @@ class CustomTimerFragment: BindingFragment<FragmentCustomTimerBinding>(R.layout.
             }
             setSubButton(R.drawable.ic_menu, onClick = { showMenu() })
         }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().previousBackStackEntry?.savedStateHandle?.set(TimerListFragment.REFRESH_KEY, true)
+                findNavController().popBackStack()
+            }
+        })
 
         context?.let {
             val notificationUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
