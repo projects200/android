308a9e065f2c9d960f7a30a696c05bea2eba65a9
edv-Shin <edvedv613@gmail.com>
2025-09-21 04:13:31 +0900

    feat: ktlint 문법 수정 #308

--- feature/matching/src/main/java/com/project200/feature/matching/MatchingMapFragment.kt
+++ feature/matching/src/main/java/com/project200/feature/matching/MatchingMapFragment.kt
@@ -5,12 +5,11 @@ import com.project200.presentation.base.BindingFragment
 import com.project200.undabang.feature.matching.R
 import com.project200.undabang.feature.matching.databinding.FragmentMatchingMapBinding
 
-class MatchingMapFragment: BindingFragment<FragmentMatchingMapBinding>(R.layout.fragment_matching_map) {
+class MatchingMapFragment : BindingFragment<FragmentMatchingMapBinding>(R.layout.fragment_matching_map) {
     override fun getViewBinding(view: View): FragmentMatchingMapBinding {
         return FragmentMatchingMapBinding.bind(view)
     }
 
     override fun setupViews() {
-
     }
 }
