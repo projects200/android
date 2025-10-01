d589c798e260ff6772e02d401d2674b20295fab1
edv-Shin <edvedv613@gmail.com>
2025-10-02 03:18:13 +0900

    feat: 매칭 카카오 오픈채팅 url 입력 화면 구현 #320

--- app/src/main/java/com/project200/undabang/main/MainActivity.kt
+++ app/src/main/java/com/project200/undabang/main/MainActivity.kt
@@ -134,6 +134,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationController {
                 com.project200.undabang.feature.matching.R.id.exercisePlaceSearchFragment,
                 com.project200.undabang.feature.matching.R.id.exercisePlaceRegisterFragment,
                 com.project200.undabang.feature.matching.R.id.matchingGuideFragment,
+                com.project200.undabang.feature.matching.R.id.matchingUrlFormFragment,
                 // ... 필요한 다른 프래그먼트 ID들 추가 ... //
             )
 
