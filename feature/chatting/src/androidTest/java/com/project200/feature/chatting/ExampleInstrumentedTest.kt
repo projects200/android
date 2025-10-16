79ae8685b91f19f130655282377d7f750f751d47
edv-Shin <edvedv613@gmail.com>
2025-10-17 01:24:31 +0900

    fix: androidTest 오류 수정

--- feature/chatting/src/androidTest/java/com/project200/feature/chatting/ExampleInstrumentedTest.kt
+++ feature/chatting/src/androidTest/java/com/project200/feature/chatting/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.project200.feature.chatting.test", appContext.packageName)
+        assertEquals("com.project200.undabang.feature.chatting.test", appContext.packageName)
     }
 }
--- feature/matching/src/androidTest/java/com/project200/feature/matching/MapTest.kt
+++
@@ -1,5 +0,0 @@
-package com.project200.feature.matching
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.CLASS)
-annotation class MapTest
