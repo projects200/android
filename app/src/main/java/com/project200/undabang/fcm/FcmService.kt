8a020a27ccffcd2dad5a03b526ad94e2b56cd82d
edv-Shin <edvedv613@gmail.com>
2025-11-16 16:46:29 +0900

    feat: fcm 채팅 헤드업 알림 구현 #384

--- app/src/main/java/com/project200/undabang/fcm/FcmService.kt
+++ app/src/main/java/com/project200/undabang/fcm/FcmService.kt
@@ -1,14 +1,24 @@
 package com.project200.undabang.fcm
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
+import androidx.core.app.NotificationCompat
 import androidx.core.content.edit
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.project200.common.constants.FcmConstants.KEY_FCM_TOKEN
 import com.project200.common.utils.EncryptedPrefs
+import com.project200.undabang.fcm.FcmConstant.CHAT_NOTI_CHANNEL_ID
+import com.project200.undabang.fcm.FcmConstant.CHAT_NOTI_CHANNEL_NAME
+import com.project200.undabang.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 import javax.inject.Inject
+import androidx.core.net.toUri
 
 @AndroidEntryPoint
 class FcmService : FirebaseMessagingService() {
@@ -43,7 +53,54 @@ class FcmService : FirebaseMessagingService() {
 
         // 백엔드에서 보낸 데이터 페이로드(Key-Value)를 확인합니다.
         if (remoteMessage.data.isNotEmpty()) {
-            Timber.tag(TAG).d("Data Payload: ${remoteMessage.data}")
+            sendNotification(remoteMessage.data)
+        }
+    }
+
+    private fun sendNotification(data: Map<String, String>) {
+        val chatRoomId = data["chatRoomId"]
+        val nickname = data["nickname"]
+        val memberId = data["memberId"]
+
+        if (chatRoomId == null || nickname == null || memberId == null) return
+
+        // 알림 식별을 위한 고유 ID
+        val uniqueId = chatRoomId.hashCode()
+
+        // 채팅방으로 이동할 딥링크 URI 생성
+        val deepLinkUri = "app://chatting/room/$chatRoomId/$nickname/$memberId".toUri()
+
+        // 클릭 시 이동할 Activity 설정
+        val intent = Intent(Intent.ACTION_VIEW, deepLinkUri).apply {
+            `package` = this@FcmService.packageName
+        }
+
+        // PendingIntent 생성
+        val pendingIntent = PendingIntent.getActivity(
+            this, uniqueId, intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        // 알림 생성
+        val notificationBuilder = NotificationCompat.Builder(this, CHAT_NOTI_CHANNEL_ID)
+            .setSmallIcon(com.project200.undabang.presentation.R.drawable.app_icon) // 알림 아이콘
+            .setContentTitle(data["nickname"]) // 알림 제목 (닉네임)
+            .setContentText(data["content"]) // 알림 본문 (내용)
+            .setAutoCancel(true) // 클릭 시 알림 자동 삭제
+            .setPriority(NotificationCompat.PRIORITY_HIGH) // 헤드업 알림을 위한 우선순위 설정
+            .setContentIntent(pendingIntent) // 클릭 시 이동할 인텐트 설정
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+
+        // 채널 생성 및 알림 발송
+        notificationManager.apply {
+            createNotificationChannel(NotificationChannel(
+                CHAT_NOTI_CHANNEL_ID,
+                CHAT_NOTI_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH // 헤드업 알림을 위한 중요도 설정
+            ))
+            notify(uniqueId, notificationBuilder.build())
         }
     }
 
