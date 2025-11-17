f7991c996804b00087c9c9b8236600539554012c
edv-Shin <edvedv613@gmail.com>
2025-11-18 03:57:11 +0900

    feat: 디버그 모드에서는 Crashlytics 비활성화 #387

--- app/src/main/java/com/project200/undabang/ApplicationClass.kt
+++ app/src/main/java/com/project200/undabang/ApplicationClass.kt
@@ -5,6 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.kakao.vectormap.KakaoMapSdk
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
@@ -22,6 +23,9 @@ class ApplicationClass : Application() {
         createNotificationChannel()
 
         KakaoMapSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+
+        // Crashlytics 디버그 모드에서는 비활성화
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
     }
 
     /**
