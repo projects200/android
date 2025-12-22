6f73b4721e7394881ebb4848f5d59b5b491e1c71
edv-Shin <edvedv613@gmail.com>
2025-12-23 04:51:02 +0900

    fix: ktlint 문법 수정 #286

--- presentation/src/main/java/com/project200/presentation/utils/TimeEditTextLimiter.kt
+++ presentation/src/main/java/com/project200/presentation/utils/TimeEditTextLimiter.kt
@@ -9,16 +9,30 @@ object TimeEditTextLimiter {
      * EditText 입력 중 최대값을 초과하지 않도록 제한하는 확장 함수
      */
     fun EditText.addRangeLimit(max: Int) {
-        this.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-            override fun afterTextChanged(s: Editable?) {
-                val input = s.toString().toIntOrNull() ?: return
-                if (input > max) {
-                    setText(max.toString())
-                    setSelection(text.length)
+        this.addTextChangedListener(
+            object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int,
+                ) {}
+
+                override fun onTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    before: Int,
+                    count: Int,
+                ) {}
+
+                override fun afterTextChanged(s: Editable?) {
+                    val input = s.toString().toIntOrNull() ?: return
+                    if (input > max) {
+                        setText(max.toString())
+                        setSelection(text.length)
+                    }
                 }
-            }
-        })
+            },
+        )
     }
 }
