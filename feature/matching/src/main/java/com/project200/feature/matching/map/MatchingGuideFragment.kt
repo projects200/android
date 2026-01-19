f7b89d4ea1062bbaf79d8721fddd21877b357c5a
edv-Shin <edvedv613@gmail.com>
2026-01-20 00:07:19 +0900

    fix: ktlint 문법 수정 #439

--- feature/matching/src/main/java/com/project200/feature/matching/map/MatchingGuideFragment.kt
+++ feature/matching/src/main/java/com/project200/feature/matching/map/MatchingGuideFragment.kt
@@ -1,22 +1,20 @@
 package com.project200.feature.matching.map
 
 import android.content.SharedPreferences
+import androidx.core.content.edit
 import androidx.navigation.fragment.findNavController
 import com.project200.common.utils.DefaultPrefs
 import com.project200.presentation.base.BindingFragment
 import com.project200.undabang.feature.matching.R
 import com.project200.undabang.feature.matching.databinding.FragmentMatchingGuideBinding
-import androidx.core.content.edit
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MatchingGuideFragment: BindingFragment<FragmentMatchingGuideBinding> (R.layout.fragment_matching_guide) {
-
+class MatchingGuideFragment : BindingFragment<FragmentMatchingGuideBinding> (R.layout.fragment_matching_guide) {
     @Inject
     @DefaultPrefs
     lateinit var sharedPreferences: SharedPreferences
-
 
     override fun getViewBinding(view: android.view.View): FragmentMatchingGuideBinding {
         return FragmentMatchingGuideBinding.bind(view)
@@ -24,14 +22,14 @@ class MatchingGuideFragment: BindingFragment<FragmentMatchingGuideBinding> (R.la
 
     override fun setupViews() {
         binding.registerBtn.setOnClickListener {
-            sharedPreferences.edit() { putBoolean(KEY_FIRST_MATCHING_VISIT, false) }
+            sharedPreferences.edit { putBoolean(KEY_FIRST_MATCHING_VISIT, false) }
             findNavController().navigate(
                 MatchingGuideFragmentDirections.actionMatchingGuideFragmentToExercisePlaceSearchFragment(),
             )
         }
 
         binding.skipBtn.setOnClickListener {
-            sharedPreferences.edit() { putBoolean(KEY_FIRST_MATCHING_VISIT, false) }
+            sharedPreferences.edit { putBoolean(KEY_FIRST_MATCHING_VISIT, false) }
             findNavController().navigateUp()
         }
     }
